12d5ab5b68fa35ea437a0db15441a4190b2ffec5
永田竜椰 <2011140077d@ed.fuk.kindai.ac.jp>
2022-11-09 18:19:34 +0000

    ログイン機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+    validates :password, presence: true, confirmation: true
+    validates :uname, presence: true
+    validates :email, presence: true
+
+
+    attr_accessor :password, :password_confirmation
+    def password=(val)
+        if val.present?
+            self.pass = BCrypt::Password.create(val)
+        end
+    @password = val
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+    resources:threads
+    resources:users, only: [:new, :create]
+    get 'users/new'
+    get 'top/login'
+    post 'top/login'
+    get 'top/logout'
+
+    root 'threads#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
