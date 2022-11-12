55fe6d349ad3ae1eae88fff79fdae4e7ec019e06
永田竜椰 <2011140077d@ed.fuk.kindai.ac.jp>
2022-11-12 12:40:43 +0000

    スレッド機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
+  
+
   
   def new
     @users = User.new
@@ -18,17 +17,15 @@ class UsersController < ApplicationController
       session[:pass] = params[:pass]
       redirect_to root_path
   
-    
     else 
     render new_user_path
-    
     end
+    
   end
   
   def destroy
     @users = User.find(params[:id])
     @users.destroy
-    flash[:notice] = 'ユーザーを削除しました'
     redirect_to root_path
   end
   
