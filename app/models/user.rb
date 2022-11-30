020f275dc021174bddbed743ad556a23bf69a3e0
永田竜椰 <2011140077d@ed.fuk.kindai.ac.jp>
2022-11-30 07:18:28 +0000

    ユーザー一覧表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
-    validates :password, presence: true, confirmation: true
+    validates :password, presence: true,confirmation: true
     validates :uname, presence: true
-    validates :email, presence: true
+    validates :email, presence: true, uniqueness: true
 
 
     attr_accessor :password, :password_confirmation
+    
     def password=(val)
         if val.present?
             self.pass = BCrypt::Password.create(val)
