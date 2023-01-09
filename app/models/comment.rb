7f8b73147fc86790ffe008a1ac272d34c32dcfc5
永田竜椰 <2011140077d@ed.fuk.kindai.ac.jp>
2023-01-09 11:54:43 +0000

    検索機能、ヘッダー、ブログ投稿機能

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,5 @@ class Comment < ApplicationRecord
     #belongs_to :user
     validates :thre, presence: true
     validates :comment, presence: true, length: { in: 1..1000 }
+    
 end
--- app/models/thre.rb
+++ app/models/thre.rb
@@ -1,4 +1,8 @@
 class Thre < ApplicationRecord
     has_many :comments, dependent: :destroy
     validates :title, presence: true
+    
+    def self.search(keyword)
+      where(["title like?", "%#{keyword}%"])
+    end
 end
