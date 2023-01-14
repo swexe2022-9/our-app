b662db352c6127303f2b3856d5cd4d2fe7d51986
永田竜椰 <2011140077d@ed.fuk.kindai.ac.jp>
2023-01-14 16:03:34 +0000

    blog機能と見た目の編集色々

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,9 @@
 class Blog < ApplicationRecord
-    
+    validates :title, presence: true
+    validates :body, presence: true
+
+    def self.search_blog(keyword)
+        where(["title like? OR body like?", "%#{keyword}%", "%#{keyword}%"])
+    end
     
 end
